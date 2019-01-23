5c4768a6c3d154c66c599aaa93b8adf628d19e84
wbakerpi <46546512+wbakerpi@users.noreply.github.com>
2019-01-22 16:23:41 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,80 @@
+<html>
+	<head>
+		<title>
+			Coding5K
+		</title>
+		<link rel="stylesheet" href="styles.css">
+	</head>
+	<body>
+		<div id="top">
+			<div id="title">
+				<h1>This is my Coding5K Project.</h1>
+			</div>
+			<p>This website will store my projects for the Coding5k Course.</p>
+		</div>
+		<br>
+		<div id="projects">
+			<a href="Day1/Postcard/index.html">
+			<div class="project">
+				<img src="img/project1.PNG"><br>
+				<h2>Project 1: Postcard</h2>
+			</div>
+			</a>
+			
+			<a href="Samples/Project2/index.html">
+			<div class="project">
+				<img src="img/project2.PNG"><br>
+				<h2>Project 2: Tell a Story</h2>
+			</div>
+			</a>
+			
+			<a href="Samples/Project3/index.html">
+			<div class="project">
+				<img src="img/project3.PNG"><br>
+				<h2>Project 3: Wanted Poster</h2>
+			</div>
+			</a>
+			
+			<a href="Samples/Project4/index.html">
+			<div class="project">
+				<img src="img/project4.PNG"><br>
+				<h2>Project 4: Recipe</h2>
+			</div>
+			</a>
+			
+			<a href="Samples/Project5/index.html">
+			<div class="project">
+				<img src="img/project5.PNG"><br>
+				<h2>Project 5: Mystery Letter</h2>
+			</div>
+			</a>
+			
 
+			<a href="Temporary/index.html">
+			<div class="project">
+				<img src="img/placeholder.jpg"><br>
+				<h2>Project 6:</h2>
+			</div>
+			</a>
+
+			<a href="Temporary/index.html">
+			<div class="project">
+				<img src="img/placeholder.jpg"><br>
+				<h2>Project 7:</h2>
+			</div>
+			</a>
+
+			<a href="Temporary/index.html">
+			<div class="project">
+				<img src="img/placeholder.jpg"><br>
+				<h2>Project 8:</h2>
+			</div>
+			</a>
+
+
+			
+		</div>
+		<br>
+		<a id="templates" href="templates.html">Templates</a>
+	</body>
+</html>
